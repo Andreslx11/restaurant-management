c47f89fbdc8d5cd5fdf1d784390f3a134b337e6a
Andres Muñoz Taborda <andreslx11@gmail.com>
2024-08-15 17:36:28 -0500

    Start  project

---
+++ src/main/java/com/company/restaurantmanagemant/RestaurantManagemantApplication.java
@@ -0,0 +1,13 @@
+package com.company.restaurantmanagemant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantManagemantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantManagemantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/restaurantmanagemant/RestaurantManagemantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.restaurantmanagemant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantManagemantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
