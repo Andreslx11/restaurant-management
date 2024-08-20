c11988e90cf6f1448837e886950e854ef2b6be49
Andres Muñoz Taborda <andreslx11@gmail.com>
2024-08-20 18:08:56 -0500

    add Category  Entity

---
+++ src/main/java/com/company/restaurantmanagemant/persistence/entity/Category.java
@@ -0,0 +1,41 @@
+package com.company.restaurantmanagemant.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+
+// Lombok annotations
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+// JPA annotations
+@Entity
+@Table(name = "categories")
+public class Category implements Serializable {
+
+    // Attributes
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+
+    @Column(name = "url_key")
+    private String urlKey;
+
+    private String state;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+}
