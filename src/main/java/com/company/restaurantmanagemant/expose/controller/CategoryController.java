bcc801a4ac17519820704749e4b98eb3e008725b
Andres Muñoz Taborda <andreslx11@gmail.com>
2024-08-21 15:36:28 -0500

    add Category  Repository and controller

---
+++ src/main/java/com/company/restaurantmanagemant/expose/controller/CategoryController.java
@@ -0,0 +1,45 @@
+package com.company.restaurantmanagemant.expose.controller;
+
+
+import com.company.restaurantmanagemant.persistence.entity.Category;
+import com.company.restaurantmanagemant.persistence.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+
+// Spring Stereotype annotation
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+
+
+    private CategoryRepository categoryRepository;
+
+
+    public CategoryController(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @GetMapping
+    public List<Category> findAll(){
+
+        List<Category> categories = (List<Category>) categoryRepository.findAll();
+
+        return  categories;
+    }
+
+
+    @GetMapping("/{id}")
+    public Category findId( @PathVariable("id") Long id){
+
+        return categoryRepository.findById(id).orElse(null);
+    }
+
+}
---
+++ src/main/java/com/company/restaurantmanagemant/persistence/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.company.restaurantmanagemant.persistence.repository;
+
+
+import com.company.restaurantmanagemant.persistence.entity.Category;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+
+}
